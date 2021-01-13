d39d468898a88d3471400a0c371bb49f6a2ae188
Don Welch <dwelch@nait.ca>
2021-01-13 11:45:53 -0700

    Create Aritmetic Sample program, demonstrate various writeline techniques using varaibles

--- FirstSampleSolution/FirstSample/Program.cs
+++ FirstSampleSolution/FirstSample/Program.cs
@@ -9,11 +9,14 @@ namespace FirstSample
             // the WriteLine() command will output the
             //    the to the console window.
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This course sections is:\tOA03 ");
+            Console.WriteLine("This course section is:\tOA03 ");
             Console.WriteLine("Your instructor name is:\tDon Welch");
 
             string myName = "Don Welch";
             Console.WriteLine($"This name is printed from a variable is:\t{myName}");
+
+
+
         }
     }
 }
