1c34ab415ed64eb70b1b8f4b168ed202269eccee
Don Welch <dwelch@nait.ca>
2021-02-12 10:38:54 -0700

    Bacterial looping problem

--- IterationPracticeSolution/IterationPractice/Program.cs
+++ IterationPracticeSolution/IterationPractice/Program.cs
@@ -123,88 +123,128 @@ namespace IterationPractice
             //investiment loop will be a pre-test loop using for()
             //output will demonstration formating of values and columns
 
-            decimal myPrincipal = 0.0m;  //the m "types" this numeric as a decimal
-            decimal myMonthlyInterestRate = 0.0m;
-            Int32 myInvestmentTime = 0; //Int32 is equalivant to int
+            //decimal myPrincipal = 0.0m;  //the m "types" this numeric as a decimal
+            //decimal myMonthlyInterestRate = 0.0m;
+            //Int32 myInvestmentTime = 0; //Int32 is equalivant to int
 
-            string menuChoice = "";
+            //string menuChoice = "";
 
-            do
+            //do
+            //{
+            //    Console.WriteLine("Welcome to CPSC Investments:\n\n");
+            //    Console.WriteLine("a) investment:");
+            //    Console.WriteLine("x) to exit\n");
+            //    Console.Write("Enter your menu choice:\t");
+            //    menuChoice = Console.ReadLine();
+
+            //    switch(menuChoice.ToUpper())
+            //    {
+            //        case "A":
+            //            {
+            //                //for this example, I will assume valid data is entered
+            //                Console.Write("\nEnter your principal investment amount:\t");
+            //                inputTemp = Console.ReadLine();
+            //                myPrincipal = decimal.Parse(inputTemp);
+            //                Console.Write("\nEnter your investment monthly rate (3% -> 0.03):\t");
+            //                inputTemp = Console.ReadLine();
+            //                myMonthlyInterestRate = decimal.Parse(inputTemp);
+            //                Console.Write("\nEnter your investment period in months:\t");
+            //                inputTemp = Console.ReadLine();
+            //                myInvestmentTime = Int32.Parse(inputTemp);
+
+            //                //use a loop for a fix amount of iterations
+            //                //best candidate would be a pre-test loop
+            //                // a) While with a counter
+            //                // b) for ( ...) loop  my choice!!!!!!!!!!
+            //                for(int monthIterator = 0; monthIterator < myInvestmentTime; monthIterator++)
+            //                {
+            //                    // the {0} is referred to as a placeholder
+            //                    // the string.Format(format pattern, value for the pattern)
+            //                    // pattern is currency and the 0 indicates a placeholder for the value
+            //                    //    in the .Format method)
+            //                    Console.Write("\nOpening; {0}\t", string.Format("{0:c}", myPrincipal));
+
+            //                    //.ToString("pattern")
+            //                    //# indicates a digit position and is optional, printed if NOT zero (0)
+            //                    //0 indicates a digit position and is required, zeroes are printed
+            //                    Console.Write("Interest Paid: {0}\t",
+            //                        (myPrincipal * myMonthlyInterestRate).ToString("$###,##0.00"));
+
+            //                    myPrincipal += myPrincipal * myMonthlyInterestRate;
+
+            //                    //{variable,xcolumnwidth:pattern}
+            //                    // variable is myPrincipal
+            //                    // x column width is 15 spaces, positive value is right align, negative
+            //                    //       value is left align
+            //                    //c stands for currency with a $ sign
+            //                    Console.Write($"Closing: {myPrincipal,15:c}\n");
+            //                    //Console.Write($"Closing: {myPrincipal,15:$###,##0.00}\n");
+            //                }
+
+            //                Console.WriteLine($"Closing {myPrincipal.ToString("c"),20}");
+            //                break;
+            //            }
+            //        case "x":
+            //            {
+            //                Console.WriteLine("\nThank you. Good-Bye.\n");
+            //                break;
+            //            }
+            //        default:
+            //            {
+            //                Console.WriteLine("\nYou entry for the menu choice is invalid. Try again.\n");
+            //                break;
+            //            }
+            //    }
+            //} while (menuChoice.ToLower() != "x");
+            //string msg = "Good luck on your future investments.";
+            //for(int i = 0; i < msg.Length; i++)
+            //{
+            //    Console.Write("*");
+            //}
+            //Console.WriteLine($"\n{msg}\n");
+            //for (int i = 0; i < msg.Length; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+
+            //Bacterial Looping Problem
+
+            double finalPopulation = 0.0;
+            double initialPopulation = 0.0;
+            double growthRate = 0.0;
+            const int MAXDAYS = 10; //PascalCase is also used for constants MaxDays
+
+            //will need the Math library value for e (2.71828)
+            Console.Write("Enter your initial bacterial population:\t");
+            inputTemp = Console.ReadLine();
+            if (double.TryParse(inputTemp, out initialPopulation))
             {
-                Console.WriteLine("Welcome to CPSC Investments:\n\n");
-                Console.WriteLine("a) investment:");
-                Console.WriteLine("x) to exit\n");
-                Console.Write("Enter your menu choice:\t");
-                menuChoice = Console.ReadLine();
-
-                switch(menuChoice.ToUpper())
+                Console.Write("Enter your bacterial growth rate as a percentage (3% -> 3.0):\t");
+                inputTemp = Console.ReadLine();
+                if (double.TryParse(inputTemp, out growthRate))
                 {
-                    case "A":
-                        {
-                            //for this example, I will assume valid data is entered
-                            Console.Write("\nEnter your principal investment amount:\t");
-                            inputTemp = Console.ReadLine();
-                            myPrincipal = decimal.Parse(inputTemp);
-                            Console.Write("\nEnter your investment monthly rate (3% -> 0.03):\t");
-                            inputTemp = Console.ReadLine();
-                            myMonthlyInterestRate = decimal.Parse(inputTemp);
-                            Console.Write("\nEnter your investment period in months:\t");
-                            inputTemp = Console.ReadLine();
-                            myInvestmentTime = Int32.Parse(inputTemp);
-
-                            //use a loop for a fix amount of iterations
-                            //best candidate would be a pre-test loop
-                            // a) While with a counter
-                            // b) for ( ...) loop  my choice!!!!!!!!!!
-                            for(int monthIterator = 0; monthIterator < myInvestmentTime; monthIterator++)
-                            {
-                                // the {0} is referred to as a placeholder
-                                // the string.Format(format pattern, value for the pattern)
-                                // pattern is currency and the 0 indicates a placeholder for the value
-                                //    in the .Format method)
-                                Console.Write("\nOpening; {0}\t", string.Format("{0:c}", myPrincipal));
-
-                                //.ToString("pattern")
-                                //# indicates a digit position and is optional, printed if NOT zero (0)
-                                //0 indicates a digit position and is required, zeroes are printed
-                                Console.Write("Interest Paid: {0}\t",
-                                    (myPrincipal * myMonthlyInterestRate).ToString("$###,##0.00"));
-
-                                myPrincipal += myPrincipal * myMonthlyInterestRate;
-
-                                //{variable,xcolumnwidth:pattern}
-                                // variable is myPrincipal
-                                // x column width is 15 spaces, positive value is right align, negative
-                                //       value is left align
-                                //c stands for currency with a $ sign
-                                Console.Write($"Closing: {myPrincipal,15:c}\n");
-                                //Console.Write($"Closing: {myPrincipal,15:$###,##0.00}\n");
-                            }
-
-                            Console.WriteLine($"Closing {myPrincipal.ToString("c"),20}");
-                            break;
-                        }
-                    case "x":
-                        {
-                            Console.WriteLine("\nThank you. Good-Bye.\n");
-                            break;
-                        }
-                    default:
-                        {
-                            Console.WriteLine("\nYou entry for the menu choice is invalid. Try again.\n");
-                            break;
-                        }
+                    growthRate /= 100.0;
+                    //we have a known number of iterations
+                    //create column headers and report title
+                    Console.WriteLine($"Bacterial Growth rate for {initialPopulation} on {MAXDAYS} growth\n");
+                    Console.WriteLine("{0,5} {1,25}", "Day", "Number of Bacteria Present");
+                    for (int i = 1; i <= MAXDAYS; i++)
+                    {
+                        finalPopulation = initialPopulation * Math.Pow(Math.E, (growthRate * (double)i));
+                        //Console.WriteLine($"{i,5} {finalPopulation,25:0.000}");
+                        Console.WriteLine("{0,5} {1,25:0.000}", i, finalPopulation);
+                    }
                 }
-            } while (menuChoice.ToLower() != "x");
-            string msg = "Good luck on your future investments.";
-            for(int i = 0; i < msg.Length; i++)
-            {
-                Console.Write("*");
+                else
+                {
+                    Console.WriteLine($"Invalid growth rate input {inputTemp}; not a valid numeric");
+
+                }
             }
-            Console.WriteLine($"\n{msg}\n");
-            for (int i = 0; i < msg.Length; i++)
+            else
             {
-                Console.Write("*");
+                Console.WriteLine($"Invalid population input {inputTemp}; not a valid numeric");
             }
         }
     }
