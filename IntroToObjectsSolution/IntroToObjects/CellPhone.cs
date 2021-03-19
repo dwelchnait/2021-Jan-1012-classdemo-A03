bba5a9750d7cc54747aa48e759126f89faa8010c
Don Welch <dwelch@nait.ca>
2021-03-19 11:40:24 -0600

    Classess: constructors and behaviours

---
+++ IntroToObjectsSolution/IntroToObjects/CellPhone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntroToObjects
+{
+    public class CellPhone
+    {
+        //Data Members
+
+        //Properties
+        //both Fully and Auto implemented properties
+
+        //Constructors
+
+        //Behaviour
+    }
+}
